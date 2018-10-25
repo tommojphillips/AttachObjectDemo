3f9faedebffb45c48509592f48c619e46b1ad597
Tommo J. Phillips <tommojphillips@gmail.com>
2018-10-25 12:48:26 +1100

    Updated to demo mod api version, 0.1.1.4

--- AttachObjectDemo/AttachObjectDemo.cs
+++ AttachObjectDemo/AttachObjectDemo.cs
@@ -1,60 +1,43 @@
-﻿using MSCLoader;
+﻿using UnityEngine;
+using MSCLoader;
+using ModApi.Attachable;
 
 namespace AttachObjectDemo
 {
     public class AttachObjectDemo : Mod
     {
-        // Written, 08.08.2018
-
-        #region Mod Fields
+        // Written, 25.10.2018
 
         public override string ID => "AttachObjectDemo"; //Your mod ID (unique)
         public override string Name => "Attach Object Demo"; //You mod name
         public override string Author => "tommojphillips"; //Your Username
         public override string Version => "1.0"; //Version
-        // Set this to true if you will be load custom assets from Assets folder.
-        // This will create subfolder in Assets folder for your mod.
         public override bool UseAssetsFolder => true;
-
-        #endregion
-
-        #region Properties / Fields
-
         private TruckEngine truckEngine;
-
-        #endregion
-
-        #region Constructors
-
-        public AttachObjectDemo()
-        {
-            // Written, 08.08.2018
-           
-        }
-
-        #endregion
-
-        #region Mod Methods
 
         public override void OnLoad()
         {
-            // Written, 08.08.2018
-            // Called once, when mod is loading after game is fully loaded            
+            // Written, 18.10.2018
+            // Called once, when mod is loading after game is fully loaded
 
-            // intializing the assemble/disassemble sounds | REQUIRED
-            ModAPI.ModAPI.intializeAssembleSounds();
-            // Initializing a new truck engine.
-            this.truckEngine = new TruckEngine(this);
-            
+            // Creating truck engine object. NOTE Mod-API excepts the gameobject to have both a rigidbody and a collider attached before passing the reference.
+            GameObject truckEngineGo = LoadAssets.LoadOBJ(this, @"truck_engine.obj", true, true);
+            truckEngineGo.name = "Truck Engine";
+            // Loading and assigning the texture for the object.
+            Texture2D engineTexture = LoadAssets.LoadTexture(this, "Truck_Engine_Texture.png");
+            truckEngineGo.GetComponent<Renderer>().material.mainTexture = engineTexture;
+            GameObject parent = GameObject.Find("SATSUMA(557kg, 248)");
+            // Creating trigger for truck engine. and assigning the local location and scale of the trigger related to the parent. (in this case the satsuma).
+            Trigger trigger = new Trigger("TruckEngineTrigger", parent, new Vector3(0.0f, 1.3f, -0.3f), Quaternion.Euler(0, 0, 0), new Vector3(1f, 1f, 1f), false);
+            // Creating a new instance of the truckengine.
+            this.truckEngine = new TruckEngine(
+                null, // The save data, as this is null, default save info will always be loaded.
+                truckEngineGo, // The instance of the part to create.
+                parent, // The parent for the part; the gameobject the part installs to.
+                trigger, // The trigger for the part.
+                new Vector3(0.0f, 0.63f, 1.5f), // The installed-position
+                new Quaternion(0, 90, 90, 0)); // The installed-rotation 
             ModConsole.Print(string.Format("{0} v{1}: Loaded.", this.Name, this.Version));
         }
-
-        public override void Update()
-        {
-            // You need to update the truckengine every update. (as of testing)
-            this.truckEngine.update(); // Testing
-        }
-
-        #endregion
     }
 }
--- AttachObjectDemo/TruckEngine.cs
+++ AttachObjectDemo/TruckEngine.cs
@@ -1,7 +1,5 @@
-﻿using ModAPI.Objects;
-using ModAPI.Triggers;
-using MSCLoader;
-using UnityEngine;
+﻿using UnityEngine;
+using ModApi.Attachable;
 
 namespace AttachObjectDemo
 {
@@ -10,68 +8,35 @@ namespace AttachObjectDemo
     /// </summary>
     public class TruckEngine : Part
     {
-        // Written, 10.08.2018
-        
-        #region Properties
+        public TruckEngine(PartSaveInfo inPartSaveInfo, GameObject part, GameObject parent, Trigger inPartTrigger, Vector3 inPartPosition, Quaternion inPartRotation) : base(inPartSaveInfo, part, parent, inPartTrigger, inPartPosition, inPartRotation)
+        {
+        }
 
-        /// <summary>
-        /// Represents the truck engine gameobject.
-        /// </summary>
-        public GameObject truckEngine
+        protected override PartSaveInfo defaultPartSaveInfo => new PartSaveInfo()
+        {
+            installed = false,
+            position = new Vector3(-20.7f, 10, 10.9f),
+            rotation = Quaternion.Euler(0, 90, 90),
+        };
+        protected override GameObject rigidPart
         {
             get;
-            private set;
+            set;
         }
-        /// <summary>
-        /// Represents the texture for the engine.
-        /// </summary>
-        private Texture2D engineTexture
+        protected override GameObject activePart
         {
             get;
             set;
         }
 
-        #endregion
-
-        #region Constructors
-
-        /// <summary>
-        /// Initializes a new instance.
-        /// </summary>
-        /// <param name="mod">The mod.</param>
-        public TruckEngine(Mod mod)
+        protected override void assemble(bool startUp = false)
         {
-            // Written, 10.08.2018
-
-            // Creating truck engine object.
-            this.truckEngine = LoadAssets.LoadOBJ(mod, @"truck_engine.obj", true, true);
-            // Getting the game objects rigidbody and assigning the mass.
-            Rigidbody engineRigidbody = this.truckEngine.GetComponent<Rigidbody>();
-            engineRigidbody.mass = 50;
-            // Loading and assigning the texture for the object.
-            this.engineTexture = LoadAssets.LoadTexture(mod, "Truck_Engine_Texture.png");
-            this.truckEngine.GetComponent<Renderer>().material.mainTexture = this.engineTexture;
-            // Setting the breakforce for the gameobject. (how much force it takes to break the gameobject off the trigger).
-            this.breakForce = 20000;
-            // Naming the object. NOTE, name must follow naming convention of <ObjectName>(xxxxx) where <ObjectName> is the game objects name.
-            this.truckEngine.name = "Truck Engine(xxxxx)";
-            // Spawning the game object to home.
-            this.truckEngine.transform.position = new Vector3(-20.7f, 10, 10.9f); // Home Location (Outside Garage)
-            this.truckEngine.transform.rotation = new Quaternion(0, 90, 90, 0);
-            // Creating trigger for truck engine. and assigning the local location and scale of the trigger related to the parent. (in this case the satsuma).
-            this.parent = GameObject.Find("SATSUMA(557kg, 248)");
-            Trigger trigger = new Trigger("TruckEngineTrigger", this.parent, new Vector3(0.0f, 1.3f, -0.3f), new Vector3(1f, 1f, 1f), false);
-            // Assigning base properties (ModAPI.Objects.Part) object.
-            this.part = this.truckEngine;
-            this.makePartPickable(true);
-            this.partTrigger = trigger;
-            // You need to initialize trigger call backs AFTER you assign the part's trigger (Trigger.partTrigger).
-            this.initializeTriggerCallback();
-            // Assigning the part's installed-point (related to the parent.)
-            this.partTriggerPosition = new Vector3(0.0f, 0.63f, 1.5f);
-            this.partTriggerRotation = new Quaternion(0, 90, 90, 0);
+            base.assemble(startUp);
         }
 
-        #endregion
+        protected override void disassemble(bool startup = false)
+        {
+            base.disassemble(startup);
+        }
     }
-}
+}       
