c3bb5e44c45f6c8b1c1aacf5c78b52f9d7d774b8
Tommo J. Phillips <tommojphillips@gmail.com>
2019-03-30 17:08:48 +1100

    Updated to work with mod api v0.1.2.0. Implemented saving of truckengine state.

--- AttachObjectDemo/AttachObjectDemo.cs
+++ AttachObjectDemo/AttachObjectDemo.cs
@@ -2,7 +2,7 @@
 using MSCLoader;
 using ModApi.Attachable;
 
-namespace AttachObjectDemo
+namespace SecureCarJack
 {
     public class AttachObjectDemo : Mod
     {
@@ -11,17 +11,47 @@ namespace AttachObjectDemo
         public override string ID => "AttachObjectDemo"; //Your mod ID (unique)
         public override string Name => "Attach Object Demo"; //You mod name
         public override string Author => "tommojphillips"; //Your Username
-        public override string Version => "1.0"; //Version
+        public override string Version => "1.1"; //Version
         public override bool UseAssetsFolder => true;
-        private TruckEngine truckEngine;
 
+        /// <summary>
+        /// Represents the truck engine part.
+        /// </summary>
+        private TruckEngine truckEngine;
+        /// <summary>
+        /// Represents the save file name.
+        /// </summary>
+        private const string fileName = "truckengine_partinfosave.txt";
+        
+        /// <summary>
+        /// Loads save data.
+        /// </summary>
+        /// <returns></returns>
+        private PartSaveInfo loadSaveData()
+        {
+            // Written, 12.10.2018
+
+            try
+            {
+                return SaveLoad.DeserializeSaveFile<PartSaveInfo>(this, fileName);
+            }
+            catch (System.NullReferenceException)
+            {
+                // no save file exists.. //loading default save data.
+
+                return null;
+            }
+        }
+        /// <summary>
+        /// Occurs on game load.
+        /// </summary>
         public override void OnLoad()
         {
             // Written, 18.10.2018
             // Called once, when mod is loading after game is fully loaded
 
             // Creating truck engine object. NOTE Mod-API excepts the gameobject to have both a rigidbody and a collider attached before passing the reference.
-            GameObject truckEngineGo = LoadAssets.LoadOBJ(this, @"truck_engine.obj", true, true);
+            GameObject truckEngineGo = LoadAssets.LoadOBJ(this, @"truck_engine.obj", rigidbody: true);
             truckEngineGo.name = "Truck Engine";
             // Loading and assigning the texture for the object.
             Texture2D engineTexture = LoadAssets.LoadTexture(this, "Truck_Engine_Texture.png");
@@ -31,13 +61,22 @@ namespace AttachObjectDemo
             Trigger trigger = new Trigger("TruckEngineTrigger", parent, new Vector3(0.0f, 1.3f, -0.3f), Quaternion.Euler(0, 0, 0), new Vector3(1f, 1f, 1f), false);
             // Creating a new instance of the truckengine.
             this.truckEngine = new TruckEngine(
-                null, // The save data, as this is null, default save info will always be loaded.
+                this.loadSaveData(), // The save data, as this is null, default save info will always be loaded.
                 truckEngineGo, // The instance of the part to create.
                 parent, // The parent for the part; the gameobject the part installs to.
                 trigger, // The trigger for the part.
                 new Vector3(0.0f, 0.63f, 1.5f), // The installed-position
                 new Quaternion(0, 90, 90, 0)); // The installed-rotation 
             ModConsole.Print(string.Format("{0} v{1}: Loaded.", this.Name, this.Version));
+        }
+        /// <summary>
+        /// Occurs on game save.
+        /// </summary>
+        public override void OnSave()
+        {
+            // Written, 30.03.2019
+
+            SaveLoad.SerializeSaveFile(this, this.truckEngine.getSaveInfo(), fileName);
         }
     }
 }
--- AttachObjectDemo/TruckEngine.cs
+++ AttachObjectDemo/TruckEngine.cs
@@ -1,42 +1,42 @@
 ﻿using UnityEngine;
 using ModApi.Attachable;
 
-namespace AttachObjectDemo
+namespace SecureCarJack
 {
     /// <summary>
     /// Represents a truck engine part for satsuma.
     /// </summary>
     public class TruckEngine : Part
     {
-        public TruckEngine(PartSaveInfo inPartSaveInfo, GameObject part, GameObject parent, Trigger inPartTrigger, Vector3 inPartPosition, Quaternion inPartRotation) : base(inPartSaveInfo, part, parent, inPartTrigger, inPartPosition, inPartRotation)
+        public TruckEngine(PartSaveInfo inPartSaveInfo, GameObject inPart, GameObject inParent, Trigger inPartTrigger, Vector3 inPartPosition, Quaternion inPartRotation) : base(inPartSaveInfo, inPart, inParent, inPartTrigger, inPartPosition, inPartRotation)
         {
         }
 
-        protected override PartSaveInfo defaultPartSaveInfo => new PartSaveInfo()
+        public override PartSaveInfo defaultPartSaveInfo => new PartSaveInfo()
         {
             installed = false,
             position = new Vector3(-20.7f, 10, 10.9f),
             rotation = Quaternion.Euler(0, 90, 90),
         };
-        protected override GameObject rigidPart
+        public override GameObject rigidPart
         {
             get;
             set;
         }
-        protected override GameObject activePart
+        public override GameObject activePart
         {
             get;
             set;
         }
 
-        protected override void assemble(bool startUp = false)
+        protected override void assemble(bool inStartup = false)
         {
-            base.assemble(startUp);
+            base.assemble(inStartup);
         }
 
-        protected override void disassemble(bool startup = false)
+        protected override void disassemble(bool inStartup = false)
         {
-            base.disassemble(startup);
+            base.disassemble(inStartup);
         }
     }
 }       
