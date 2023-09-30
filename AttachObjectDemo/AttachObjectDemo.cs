1060b756a833e58190c873934e7635b8f6d8c600
Tommo J. Phillips <tommojphillips@gmail.com>
2023-09-30 14:23:59 +1000

    # Updated to work with ModAPI v0.2.0.12

--- AttachObjectDemo/AttachObjectDemo.cs
+++ AttachObjectDemo/AttachObjectDemo.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using MSCLoader;
-using ModApi.Attachable;
+using TommoJProductions.ModApi.Attachable;
+using System.IO;
+using System;
+using TommoJProductions.ModApi.Database;
 
 namespace SecureCarJack
 {
@@ -17,31 +20,12 @@ namespace SecureCarJack
         /// <summary>
         /// Represents the truck engine part.
         /// </summary>
-        private TruckEngine truckEngine;
+        private Part truckEngine;
         /// <summary>
         /// Represents the save file name.
         /// </summary>
         private const string fileName = "truckengine_partinfosave.txt";
         
-        /// <summary>
-        /// Loads save data.
-        /// </summary>
-        /// <returns></returns>
-        private PartSaveInfo loadSaveData()
-        {
-            // Written, 12.10.2018
-
-            try
-            {
-                return SaveLoad.DeserializeSaveFile<PartSaveInfo>(this, fileName);
-            }
-            catch (System.NullReferenceException)
-            {
-                // no save file exists.. //loading default save data.
-
-                return null;
-            }
-        }
         /// <summary>
         /// Occurs on game load.
         /// </summary>
@@ -50,33 +34,93 @@ namespace SecureCarJack
             // Written, 18.10.2018
             // Called once, when mod is loading after game is fully loaded
 
-            // Creating truck engine object. NOTE Mod-API excepts the gameobject to have both a rigidbody and a collider attached before passing the reference.
-            GameObject truckEngineGo = LoadAssets.LoadOBJ(this, @"truck_engine.obj", rigidbody: true);
+            // Creating truck engine object.
+            GameObject truckEngineGo = loadEngine();
+
+            GameObject parent = Database.databaseVehicles.satsuma;
+            TriggerData triggerData = TriggerData.createTriggerData("truckEngineTriggerData");
+
+            // Creating trigger for truck engine.
+            TriggerSettings triggerSettings = new TriggerSettings() 
+            {
+                triggerID = "TruckEngineTrigger",
+                triggerData = triggerData,
+                triggerPosition = new Vector3(0.0f, 1.3f, -0.3f)
+            };
+            Trigger trigger = new Trigger(parent, triggerSettings);
+
+            // Creating a new instance of the truckengine.
+            PartSettings partSettings = new PartSettings()
+            {
+                assembleType = AssembleType.joint,
+                assemblyTypeJointSettings = new AssemblyTypeJointSettings()
+                {
+                    breakForce = float.PositiveInfinity, // unbreakable joint.
+                }
+            };
+            truckEngine = truckEngineGo.AddComponent<Part>();
+            truckEngine.initPart(triggerData);
+
+            ModConsole.Print(string.Format("{0} v{1}: Loaded.", this.Name, this.Version));
+        }
+
+        private GameObject loadEngine() 
+        {
+            GameObject truckEngineGo = LoadOBJ(this, @"truck_engine.obj", rigidbody: true);
             truckEngineGo.name = "Truck Engine";
             // Loading and assigning the texture for the object.
             Texture2D engineTexture = LoadAssets.LoadTexture(this, "Truck_Engine_Texture.png");
             truckEngineGo.GetComponent<Renderer>().material.mainTexture = engineTexture;
-            GameObject parent = GameObject.Find("SATSUMA(557kg, 248)");
-            // Creating trigger for truck engine. and assigning the local location and scale of the trigger related to the parent. (in this case the satsuma).
-            Trigger trigger = new Trigger("TruckEngineTrigger", parent, new Vector3(0.0f, 1.3f, -0.3f), Quaternion.Euler(0, 0, 0), new Vector3(1f, 1f, 1f), false);
-            // Creating a new instance of the truckengine.
-            this.truckEngine = new TruckEngine(
-                this.loadSaveData(), // The save data, as this is null, default save info will always be loaded.
-                truckEngineGo, // The instance of the part to create.
-                parent, // The parent for the part; the gameobject the part installs to.
-                trigger, // The trigger for the part.
-                new Vector3(0.0f, 0.63f, 1.5f), // The installed-position
-                new Quaternion(0, 90, 90, 0)); // The installed-rotation 
-            ModConsole.Print(string.Format("{0} v{1}: Loaded.", this.Name, this.Version));
-        }
-        /// <summary>
-        /// Occurs on game save.
-        /// </summary>
-        public override void OnSave()
-        {
-            // Written, 30.03.2019
 
-            SaveLoad.SerializeSaveFile(this, this.truckEngine.getSaveInfo(), fileName);
+            return truckEngineGo;
+        }
+
+        public static GameObject LoadOBJ(Mod mod, string fileName, bool collider = true, bool rigidbody = false)
+        {
+            Mesh mesh = LoadOBJMesh(mod, fileName);
+            if (mesh != null)
+            {
+                GameObject gameObject = new GameObject();
+                gameObject.AddComponent<MeshFilter>().mesh = mesh;
+                gameObject.AddComponent<MeshRenderer>();
+                if (rigidbody)
+                {
+                    gameObject.AddComponent<Rigidbody>();
+                }
+
+                if (collider)
+                {
+                    if (rigidbody)
+                    {
+                        gameObject.AddComponent<MeshCollider>().convex = true;
+                    }
+                    else
+                    {
+                        gameObject.AddComponent<MeshCollider>();
+                    }
+                }
+
+                return gameObject;
+            }
+
+            return null;
+        }
+        public static Mesh LoadOBJMesh(Mod mod, string fileName)
+        {
+            string text = Path.Combine(ModLoader.GetModAssetsFolder(mod), fileName);
+            if (!File.Exists(text))
+            {
+                throw new FileNotFoundException("<b>LoadOBJ() Error:</b> File not found: " + text + Environment.NewLine, text);
+            }
+
+            if (Path.GetExtension(text).ToLower() == ".obj")
+            {
+                Mesh mesh = new OBJLoader().ImportFile(Path.Combine(ModLoader.GetModAssetsFolder(mod), fileName));
+                mesh.name = Path.GetFileNameWithoutExtension(text);
+                return mesh;
+            }
+
+            throw new NotSupportedException("<b>LoadOBJ() Error:</b> Only (*.obj) files are supported" + Environment.NewLine);
         }
     }
 }
